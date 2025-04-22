3655bb1a923d484579f0bd308cc6c7a981a38247
hidden-er <hiddeneer@gmail.com>
2025-04-22 16:55:58 +0800

    Update run-wpe.sh

--- run-wpe.sh
+++ run-wpe.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+export PATH="/usr/local/bin:/usr/bin:/bin"
+exec 2> /var/log/hostapd-wpe-error.log
+
+systemctl stop NetworkManager
+ifconfig wlan0 down
+ifconfig wlan0 up
 airmon-ng check kill
 hostapd-wpe /etc/hostapd-wpe/hostapd-wpe.conf
